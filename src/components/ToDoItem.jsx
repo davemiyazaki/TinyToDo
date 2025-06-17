50deab7b0bc52cc063b39ec3621a6cdab2a4c9f5
Dave Miyazaki <denixden2818@gmail.com>
2025-06-16 23:52:23 -0400

    Edited design of ToDoItem | Working on ToDoItem still

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,10 +1,16 @@
 const ToDoItem = () =>{
   return(
-    <div className="grid grid-cols-[10%_90%] w-full h-7 bg-stone-500 overflow-hidden">
-      <img className="object-cover" src="/todo-icon.jpg"/>
-      <span> To-Do Component </span>
+    <div className="grid grid-cols-[10%_90%] w-full h-fit text-sm">
+      <div className= " p-[7px] box-border h-full w-full" id="checklist">
+        <img className="object-cover border-b-black border-2 rounded-md" src="/todo-icon.jpg"/>
+      </div>
+
+
+      <div className="h-full w-full box-border p-1 justify-center" id="toDoName"> 
+        <span className = "h-full" > To-Do Component </span>
+      </div>
     </div>
   );
-}
 
+}
 export default ToDoItem;
